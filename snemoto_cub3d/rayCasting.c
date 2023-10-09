7c4092a24c5fbba2fd83afc7ef9bd9b9f3eb341f
Shota Nemoto <snemoto@c1r22s2.42tokyo.jp>
2023-10-09 15:02:56 +0900

    get_position

--- snemoto_cub3d/rayCasting.c
+++ snemoto_cub3d/rayCasting.c
@@ -6,13 +6,83 @@
 /*   By: snemoto <snemoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:27:52 by snemoto           #+#    #+#             */
-/*   Updated: 2023/10/08 11:43:09 by snemoto          ###   ########.fr       */
+/*   Updated: 2023/10/09 14:59:03 by snemoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rayCasting.h"
 
-int main(void)
+unsigned int map[MAPROW][MAPCOL]=
 {
+  {1,1,1,1,1,1},
+  {1,0,0,0,0,1},
+  {1,0,0,0,0,1},
+  {1,0,0,0,0,1},
+  {1,0,0,0,0,1},
+  {1,1,1,1,1,1}
+};
+
+t_pos	*get_pos(t_vars var)
+{
+	t_pos	*rv;
+
+	rv = (t_pos *)malloc(sizeof(t_pos));
+	rv->pos_x = var.pos->pos_x;
+	rv->pos_y = var.pos->pos_y;
+	return (rv);
+}
+
+t_pos	*get_pos_wall(t_pos *pos, unsigned int map[MAPROW][MAPCOL], t_wall wallkind)
+{
+	t_pos			*rv;
+	int				row;
+	int				col;
+
+	rv = (t_pos *)malloc(sizeof(t_pos));
+	row = pos->pos_x;
+	col = pos->pos_y;
+	while ((map[row][col] + '0') != '1' && 0 < col)
+		col--;
+	rv->pos_y = col;
+	if (wallkind == AHEAD)
+		rv->pos_x = pos->pos_x;
+	else if (wallkind == LEFT)
+		while ((map[row][col] + '0') != '0' && 0 < row)
+			row--;
+	else if (wallkind == RIGHT)
+		while ((map[row][col] + '0') != '0' && row < MAPROW - 1)
+			row++;
+	rv->pos_x = row;
+	return (rv);
+}
+
+int	main(void)
+{
+	t_vars	var;
+	t_pos	*pos;
+	t_wall	wallkind;
+	t_pos	*pos_wall_ahead;
+	t_pos	*pos_wall_left;
+	t_pos	*pos_wall_right;
+
+	var.pos->pos_x = 2;
+	var.pos->pos_y = 4;
+	var.pos->element = 0;
+
+	pos = get_pos(var);
+	printf("X;%d Y;%d\n", pos->pos_x, pos->pos_y);
+
+	wallkind = AHEAD;
+	pos_wall_ahead = get_pos_wall(pos, map, wallkind);
+	printf("X;%d Y;%d\n", pos_wall_ahead->pos_x, pos_wall_ahead->pos_y);
+
+	wallkind = LEFT;
+	pos_wall_left = get_pos_wall(pos, map, wallkind);
+	printf("X;%d Y;%d\n", pos_wall_left->pos_x, pos_wall_left->pos_y);
+
+	wallkind = RIGHT;
+	pos_wall_right = get_pos_wall(pos, map, wallkind);
+	printf("X;%d Y;%d\n", pos_wall_right->pos_x, pos_wall_right->pos_y);
+
 	return (0);
 }
--- snemoto_cub3d/rayCasting.h
+++ snemoto_cub3d/rayCasting.h
@@ -6,11 +6,54 @@
 /*   By: snemoto <snemoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:27:38 by snemoto           #+#    #+#             */
-/*   Updated: 2023/10/08 11:44:29 by snemoto          ###   ########.fr       */
+/*   Updated: 2023/10/09 14:54:05 by snemoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RAYCASTING_H
 # define RAYCASTING_H
+
+# define MAPROW 6
+# define MAPCOL 6
+
+# include <stdio.h>
+# include <stdlib.h>
+
+typedef enum e_wall
+{
+	AHEAD,
+	LEFT,
+	RIGHT,
+}	t_wall;
+
+typedef struct s_pos
+{
+	unsigned int	pos_x;
+	unsigned int	pos_y;
+	char			element;
+}	t_pos;
+
+typedef struct s_vars
+{
+	t_pos	*pos;
+}	t_vars;
+
+t_pos	*get_pos(t_vars var);
+t_pos	*get_pos_wall(t_pos *pos, unsigned int map[MAPROW][MAPCOL], t_wall wallKild);
+
+// void	get_dir(void);
+
+// void	calc_height(void);
+// void	calc_width(void);
+
+// void	draw_wall(void);
+// void	draw_ceiling(void);
+// void	draw_floor(void);
+
+// void	key_change_pos(void);
+// void	key_change_dir(void);
+
+// void	get_image(void);
+// void	draw_image(void);
 
 #endif
