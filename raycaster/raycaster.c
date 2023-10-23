8daee1b1955c4ea771a135e4babc1b2646f2ca9b
Shota Nemoto <snemoto@c1r21s10.42tokyo.jp>
2023-10-23 18:29:47 +0900

    init raycaster

--- raycaster/raycaster.c
+++ raycaster/raycaster.c
@@ -6,25 +6,60 @@
 /*   By: snemoto <snemoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:27:52 by snemoto           #+#    #+#             */
-/*   Updated: 2023/10/22 16:55:54 by snemoto          ###   ########.fr       */
+/*   Updated: 2023/10/23 18:27:29 by snemoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "rc.h"
+#include "raycaster.h"
 
-unsigned int map[ROW][COL]=
+int map[mapWidth][mapHeight]=
 {
-	{1,1,1,1,1,1},
-	{1,0,0,0,0,1},
-	{1,0,0,0,0,1},
-	{1,0,0,0,0,1},
-	{1,0,0,0,0,1},
-	{1,1,1,1,1,1}
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
+
+static void	key_draw_clear(t_n_vars *var)
+{
+	unsigned int	row;
+	unsigned int	col;
+
+	row = 0;
+	col = 0;
+	while (row++ < screenWidth)
+	{
+		col = 0;
+		while (col++ < screenWidth)
+			mlx_pixel_put(var->mlx, var->win, row, col, BLACK);			
+	}
+}
 
 static int	n_key_hook(int keycode, t_n_vars *var)
 {
 	double	sign = 1;
+	double	angle_x = 1;
+	double	angle_y = 1;
 	double	move = 1;
 	double	rot = M_PI / ANGLE;
 	double	old_dir_x = var->dir->dir_x;
@@ -34,56 +69,54 @@ static int	n_key_hook(int keycode, t_n_vars *var)
 	{
 		if (keycode == KEY_S)
 			sign = -1;
-		if (map[(int)(var->pos->pos_x + sign * var->dir->dir_x * move)][(int)var->pos->pos_x])
+		if (map[(int)(var->pos->pos_x + sign * var->dir->dir_x * move)][(int)var->pos->pos_y] == 0)
 			var->pos->pos_x += sign * var->dir->dir_x * move;
-		if (map[(int)var->pos->pos_x][(int)(var->pos->pos_y + sign * var->dir->dir_x * move)])
+		if (map[(int)var->pos->pos_x][(int)(var->pos->pos_y + sign * var->dir->dir_y * move)] == 0)
 			var->pos->pos_y += sign * var->dir->dir_y * move;
-	}
-	else if (keycode == KEY_A || keycode == KEY_D)
-	{
-		if (keycode == KEY_D)
-			sign = -1;
-		if (map[(int)(var->pos->pos_x + sign * var->dir->dir_x * move)][(int)var->pos->pos_x])
-			var->pos->pos_x += sign * var->dir->dir_x * move;
-		if (map[(int)var->pos->pos_x][(int)(var->pos->pos_y - sign * var->dir->dir_x * move)])
-			var->pos->pos_y -= sign * var->dir->dir_y * move;
 	}
 	else if (keycode == KEY_LEFT || keycode == KEY_RIGHT)
 	{
-		if (keycode == KEY_LEFT)
+		if (keycode == KEY_RIGHT)
 			rot *= -1;
 		var->dir->dir_x = var->dir->dir_x * cos(rot) - var->dir->dir_y * sin(rot);
 		var->dir->dir_y = old_dir_x * sin(rot) + var->dir->dir_y * cos(rot);
 		var->plane->plane_x = var->plane->plane_x * cos(rot) - var->plane->plane_y * sin(rot);
 		var->plane->plane_y = old_plane_x * sin(rot) + var->plane->plane_y * cos(rot);
 	}
+	else if (keycode == KEY_A || keycode == KEY_D)
+	{
+		angle_x = var->dir->dir_x * cos(M_PI / 2) - var->dir->dir_y * sin(M_PI / 2);
+		angle_y = old_dir_x * sin(M_PI / 2) + var->dir->dir_y * cos(M_PI / 2);
+		if (keycode == KEY_D)
+			sign = -1;
+		if (map[(int)(var->pos->pos_x + angle_x * sign * move)][(int)var->pos->pos_y] == 0)
+			var->pos->pos_x += angle_x * sign * move;
+		if (map[(int)var->pos->pos_x][(int)(var->pos->pos_y + angle_y * sign * move)] == 0)
+			var->pos->pos_y += angle_y * sign * move;
+	}
+	else
+		return (0);
+	key_draw_clear(var);
 	return (0);
 }
 
 static int	n_key_draw(t_n_vars *var)
 {
 	int	x = 0;
-	while (x < WINW)
+	while (x < screenWidth)
 	{
-		double camera_x = 2 * x / (double)WINW - 1;
+		double camera_x = 2 * x / (double)screenWidth - 1;
 		double ray_dir_x = var->dir->dir_x + var->plane->plane_x * camera_x;
 		double ray_dir_y = var->dir->dir_y + var->plane->plane_y * camera_x;
-
 		int		map_x = (int)var->pos->pos_x;
 		int		map_y = (int)var->pos->pos_y;
-
-		double	side_dist_x;
-		double	side_dist_y;
-
 		double	delta_dist_x = (ray_dir_x == 0) ? 1e30 : fabs(1 / ray_dir_x);
 		double	delta_dist_y = (ray_dir_y == 0) ? 1e30 : fabs(1 / ray_dir_y);
 
 		int		step_x;
 		int		step_y;
-
-		int		hit = 0;
-		int		side;
-
+		double	side_dist_x;
+		double	side_dist_y;
 		if (ray_dir_x < 0)
 		{
 			step_x = -1;
@@ -105,11 +138,13 @@ static int	n_key_draw(t_n_vars *var)
 			side_dist_y = (map_y + 1.0 - var->pos->pos_y) * delta_dist_y;
 		}
 
+		int		hit = 0;
+		int		side;
 		while (hit == 0)
 		{
 			if (side_dist_x < side_dist_y)
 			{
-				side_dist_x += delta_dist_y;
+				side_dist_x += delta_dist_x;
 				map_x += step_x;
 				side = 0;
 			}
@@ -119,38 +154,40 @@ static int	n_key_draw(t_n_vars *var)
 				map_y += step_y;
 				side = 1;
 			}
-			if (map[map_x][map_y] == 1)
+			if (map[map_x][map_y] > 0)
 				hit = 1;
 		}
 		
 		double	perp_wall_dist;
-
 		if (side == 0)
 			perp_wall_dist = side_dist_x - delta_dist_x;
 		else
 			perp_wall_dist = side_dist_y - delta_dist_y;
 		
-		int	line_height = (int)(WINH / perp_wall_dist);
-
-		int	draw_start = -line_height / 2 + WINH / 2;
+		int	line_height = (int)(screenHeight / perp_wall_dist);
+		int	draw_start = -line_height / 2 + screenHeight / 2;
 		if (draw_start < 0)
 			draw_start = 0;
-		int	draw_end = line_height / 2 + WINH / 2;
-		if (draw_end >= WINH)
-			draw_end = WINH - 1;
-		
-		int	color;
-		if (map[map_x][map_y] == 1)
-			color = RED;
-		else
-			color = BLACK;
+		int	draw_end = line_height / 2 + screenHeight / 2;
+		if (draw_end >= screenHeight)
+			draw_end = screenHeight - 1;
 
+		int	color;
+		switch (map[map_x][map_y])
+		{
+			case 1: color = RED; break ;
+			case 2: color = GREEN; break ;
+			case 3: color = BLUE; break ;
+			case 4: color = WHITE; break ;
+			default: color = PURPLE; break ;
+		}
 		if (side == 1)
 			color /= 2;
 
 		int	col = draw_start;
 		while (col < draw_end)
 			mlx_pixel_put(var->mlx, var->win, x, col++, color);
+		x++;
 	}
 	return (0);
 }
@@ -160,17 +197,19 @@ int	main(void)
 	t_n_vars	var;
 
 	var.pos = (t_n_pos *)malloc(sizeof(t_n_pos));
-	var.pos->pos_x = 2;
-	var.pos->pos_y = 3;
+	var.pos->pos_x = 22;
+	var.pos->pos_y = 12;
+
 	var.dir = (t_n_dir *)malloc(sizeof(t_n_dir));
 	var.dir->dir_x = -1;
-	var.dir->dir_x = 0;
+	var.dir->dir_y = 0;
+
 	var.plane = (t_n_plane *)malloc(sizeof(t_n_plane));
 	var.plane->plane_x = 0;
-	var.plane->plane_x = 0.66;
+	var.plane->plane_y = 0.66;
 
 	var.mlx = mlx_init();
-	var.win = mlx_new_window(var.mlx, WINW, WINH, "rc");
+	var.win = mlx_new_window(var.mlx, screenWidth, screenHeight, "rc");
 	mlx_key_hook(var.win, n_key_hook, &var);
 	mlx_loop_hook(var.mlx, n_key_draw, &var);
 	mlx_loop(var.mlx);
--- raycaster/raycaster.h
+++ raycaster/raycaster.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rc.h                                               :+:      :+:    :+:   */
+/*   raycaster.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: snemoto <snemoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:27:38 by snemoto           #+#    #+#             */
-/*   Updated: 2023/10/23 18:26:37 by snemoto          ###   ########.fr       */
+/*   Updated: 2023/10/23 18:29:15 by snemoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef RC_H
-# define RC_H
+#ifndef RAYCASTER_H
+# define RAYCASTER_H
 
 # include <stdio.h>
 # include <stdlib.h>
@@ -25,73 +25,49 @@
 # define KEY_LEFT 0xFF51
 # define KEY_RIGHT 0xFF53
 
-# define BLACK 0x000000
-# define WATER 0x00FFFF
 # define WHITE 0xFFFFFF
-
 # define RED 0xFF0000
 # define GREEN 0x00FF00
 # define BLUE 0x0000FF
 # define PURPLE 0xFF00FF
 
-# define CEILING 0xC8C8C8
-# define FLOOR 0xA1A1A1
-
-# define ROW 6
-# define COL 6
-
-extern unsigned int map[ROW][COL];
-
-# define WALL 100
-
-# define WINH WALL * 10
-# define WINW WINH * 2
-
-#define MAPL WINW / 2 - ROW / 2 * WALL
-#define MAPR WINW / 2 + ROW / 2 * WALL
-
 # define ANGLE 16
+
+#define screenWidth 1280
+#define screenHeight 960
+#define mapWidth 24
+#define mapHeight 24
 
 /* ************************************************************************** */
 
-typedef struct s_dir
+typedef struct s_n_pos
+{
+	double	pos_x;
+	double	pos_y;
+}	t_n_pos;
+
+typedef struct s_n_dir
 {
 	double	dir_x;
 	double	dir_y;
-	double	angle;
-	int		color;
-}	t_dir;
+}	t_n_dir;
 
-typedef struct s_dis
+typedef struct s_n_plane
 {
-	unsigned int	dis_x;
-	unsigned int	dis_y;
-}	t_dis;
+	double	plane_x;
+	double	plane_y;
+}	t_n_plane;
 
-typedef struct s_pos
+typedef struct s_n_vars
 {
-	t_dis			*dis;
-	unsigned int	pos_x;
-	unsigned int	pos_y;
-}	t_pos;
-
-typedef struct s_vars
-{
-	t_pos	*pos;
-	t_dir	*dir;
+	t_n_pos	*pos;
+	t_n_dir	*dir;
+	t_n_plane	*plane;
 	void	*mlx;
 	void	*win;
 	void	*img;
-}	t_vars;
+}	t_n_vars;
 
 /* ************************************************************************** */
-
-int		key_hook(int keycode, t_vars *var);
-int		key_draw(t_vars *var);
-
-void	print_pos(t_vars var);
-void	print_dir(t_vars var);
-void	key_draw_grid(t_vars *var);
-void	key_draw_map_range(t_vars *var);
 
 #endif
