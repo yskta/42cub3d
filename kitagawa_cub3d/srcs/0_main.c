d17af859d397811231f22580104d6ab063ff30b1
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2023-10-27 23:04:36 +0900

    commit

--- kitagawa_cub3d/srcs/0_main.c
+++ kitagawa_cub3d/srcs/0_main.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:20:59 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/10/27 23:03:49 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/10/27 23:04:30 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ int main(int argc, char *argv[])
         printf("%s\n", data->map_data.map[i]);
         i++;
     }
+    printf("map_height = %d\n", data->map_data.map_height);
     //render_map(data);
     //mlx_key_hook(data->mlx_win, Expose, ExposureMask, render_map, data);
 	//mlx_loop_hook(data->mlx_win, KeyPress, KeyPressMask, key_action, data);
