4646dd3f508ad4f52742e4360b60c80c64e9e388
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2023-10-28 10:40:33 +0900

    map判定(左端)

--- kitagawa_cub3d/srcs/3_3_parse_read_data.c
+++ kitagawa_cub3d/srcs/3_3_parse_read_data.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 10:22:36 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/10/27 23:46:55 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/10/28 10:22:42 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,23 +63,70 @@ bool	check_row(t_data *data)
 	return (true);
 }
 
+bool	check_line(char	**map)
+{
+	size_t	i;
+	size_t	j;
+	size_t	left_end_wall_index;
+	size_t	right_end_wall_index;
+
+	i = 0;
+	j = 0;
+	//左端を見ていく。
+	while (map[0][j] == 'X')//1行目の左端の壁のindexを探す。
+		j++;
+	left_end_wall_index = j;
+	while(map[i + 1] != NULL)//2行目以降の左端の壁
+	{
+		if (map[i + 1][left_end_wall_index] == '0')
+		{
+			//左橋の壁のindexを探す。
+			while(left_end_wall_index > 0)
+			{
+				if (map[i][left_end_wall_index - 1] == '1')
+					left_end_wall_index--;
+				else
+					break ;
+			}
+			//ここの時点で、left_end_wall_indexは左端の壁のindexになっている。
+			if (map[i + 1][left_end_wall_index] != '1')
+			{
+				printf("invalid_map\n");
+				return (false);
+			}
+			else
+				i++;
+		}
+		else if (map[i + 1][left_end_wall_index] == 'X')
+		{
+			while(map[i][left_end_wall_index + 1] == '1')
+				left_end_wall_index++;
+			if (map[i + 1][left_end_wall_index] != '1')
+			{
+				printf("invalid_map\n");
+				return (false);
+			}
+			else
+				i++;
+		}
+		else
+			i++;
+	}
+	return (true);
+	
+}
+
 bool	check_valid_map(t_data *data)
 {
 	char **copied_map;
 
 	copied_map = copy_map_contents(data);
-	int i = 0;
-	while (copied_map[i] != NULL)
-	{
-		printf("%s\n", copied_map[i]);
-		i++;
-	}
 	if(data->map_data.map[0] == NULL)
 		return (false);
-	if (check_row(data) == false)
+	if (check_row(data, copied_map) == false)
 		return (false);
-	// if (check_line(data) == false)
-	// 	return (false);
+	if (check_line(copied_map) == false)
+		return (false);
 	free_two_dimensional_array(copied_map);
 	return (true);
 }
