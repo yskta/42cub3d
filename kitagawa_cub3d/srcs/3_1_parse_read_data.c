f97c22b2463911556d7e06e5ed20b4bce08610f2
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2023-10-28 15:37:28 +0900

    commit

--- kitagawa_cub3d/srcs/3_1_parse_read_data.c
+++ kitagawa_cub3d/srcs/3_1_parse_read_data.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 15:12:57 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/10/28 15:35:49 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/10/28 15:37:23 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,7 @@ bool	judge_space_or_wall(char *line)
 
 	i = 0;
 	printf("line:%s\n", line);
+	printf("line[i]:%c\n", line[i]);
 	while (line[i] == ' ' || line[i] == '1')
 	{
 		printf("line[i]:%c\n", line[i]);
