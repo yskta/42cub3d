f2ca70a2baad9354875613d3b2f3d066e9ad7b8e
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2023-10-28 11:11:26 +0900

    デバック

--- kitagawa_cub3d/srcs/3_3_parse_read_data.c
+++ kitagawa_cub3d/srcs/3_3_parse_read_data.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 10:22:36 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/10/28 10:55:36 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/10/28 11:11:16 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -131,6 +131,13 @@ bool	check_valid_map(t_data *data)
 	char **copied_map;
 
 	copied_map = copy_map_contents(data);
+
+	int i = 0;
+	while (copied_map[i] != NULL)
+	{
+		printf("%s\n", copied_map[i]);
+		i++;
+	}
 	if(data->map_data.map[0] == NULL)
 		return (false);
 	// if (check_row(data) == false)
