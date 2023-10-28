7909f4e22f2692b0674a17416c9685721dfbd707
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2023-10-28 17:49:58 +0900

    commit

--- kitagawa_cub3d/include/cub3d.h
+++ kitagawa_cub3d/include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 18:58:57 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/10/28 15:45:24 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/10/28 17:42:51 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <string.h>
 # include <stdlib.h>
 # include <stdbool.h>
+# include <math.h>
 # include "../libft/libft.h"
 # include "../minilibx-linux/mlx.h"
 # include <X11/X.h>
@@ -49,6 +50,56 @@
 # define KEY_D 100
 # define KEY_S 115
 # define KEY_W 119
+
+//nemotoさん分追加ここから
+typedef struct s_pos
+{
+	double	pos_x;
+	double	pos_y;
+}	t_pos;
+
+typedef struct s_dir
+{
+	double	dir_x;
+	double	dir_y;
+}	t_dir;
+
+typedef struct s_plane
+{
+	double	plane_x;
+	double	plane_y;
+}	t_plane;
+
+typedef struct s_step
+{
+	int	step_x;
+	int	step_y;
+}	t_step;
+
+typedef struct s_side_dist
+{
+	double	side_dist_x;
+	double	side_dist_y;
+}	t_side_dist;
+
+typedef struct	s_map
+{
+	int	map_x;
+	int	map_y;
+}	t_map;
+
+typedef struct	s_delta_dist
+{
+	double	delta_dist_x;
+	double	delta_dist_y;
+}	t_delta_dist;
+
+typedef struct	s_ray_dir
+{
+	double	ray_dir_x;
+	double	ray_dir_y;
+}	t_ray_dir;
+//ここまで
 
 typedef struct s_map{
 	char	**read_data; //mallocする
@@ -84,6 +135,15 @@ typedef struct s_data{
 	t_texture				east;
 	t_floor_or_ceiling		floor;
 	t_floor_or_ceiling		ceiling;
+	//nemotoさん分追加ここから
+	// t_pos					*pos;
+	// t_dir					*dir;
+	// t_plane					*plane;
+	// t_step					*step;
+	// t_side_dist				*side_dist;
+	// t_delta_dist			*delta_dist;
+	// t_ray_dir				*ray_dir;
+	//ここまで
 }t_data;//mallocする
 
 bool		check_arg(int argc, char **argv);
--- kitagawa_cub3d/srcs/3_3_parse_read_data.c
+++ kitagawa_cub3d/srcs/3_3_parse_read_data.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 10:22:36 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/10/28 17:19:24 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/10/28 17:49:41 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -273,21 +273,54 @@ bool	check_num_of_player_and_invalid_char(char	**map)
 	}
 }
 
-// bool	check_playable_map(t_data *data)
-// {
-// 	size_t	i;
-// 	size_t	j;
+bool	check_playable_map(t_data *data)
+{
+	size_t	i;
+	size_t	j;
+	size_t	player_pos_x;
+	size_t	player_pos_y;
 
-// 	i = 0;
-// 	return (true);
-// }
+	i = 0;
+	while (data->map_data.map[i] != NULL)
+	{
+		j = 0;
+		while (data->map_data.map[i][j] != '\0')
+		{
+			if (data->map_data.map[i][j] == 'N' || data->map_data.map[i][j] == 'S' || data->map_data.map[i][j] == 'W' || data->map_data.map[i][j] == 'E')
+			{
+				player_pos_x = j;
+				player_pos_y = i;
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("player_pos_x = %zu\n", player_pos_x);
+	printf("player_pos_y = %zu\n", player_pos_y);
+	//playerの周りが1か0かどうかだけみれば、playableかどうかわかる。
+	if (data->map_data.map[player_pos_y - 1][player_pos_x] == '1' || data->map_data.map[player_pos_y - 1][player_pos_x] == '0')
+	{
+		if (data->map_data.map[player_pos_y + 1][player_pos_x] == '1' || data->map_data.map[player_pos_y + 1][player_pos_x] == '0')
+		{
+			if (data->map_data.map[player_pos_y][player_pos_x - 1] == '1' || data->map_data.map[player_pos_y][player_pos_x - 1] == '0')
+			{
+				if (data->map_data.map[player_pos_y][player_pos_x + 1] == '1' || data->map_data.map[player_pos_y][player_pos_x + 1] == '0')
+					return (true);
+			}
+		}
+	}
+	else
+	{
+		printf("invalid_map in playable_map\n");
+		return (false);
+	}
+}
 
 bool	check_valid_map(t_data *data)
 {
 	char **copied_map;
-
+	
 	copied_map = copy_map_contents(data);
-
 	// int i = 0;
 	// while (copied_map[i] != NULL)
 	// {
