b0d3a140ee405ead4d29488c8ce699e6f5db6fd6
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2023-11-04 00:31:02 +0900

    test

--- kitagawa_cub3d_統合_untexture_ver/srcs/3_1_parse_read_data.c
+++ kitagawa_cub3d_統合_untexture_ver/srcs/3_1_parse_read_data.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 15:12:57 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/10/28 16:09:32 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/11/04 00:30:54 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,8 +89,8 @@ bool	parse_map(t_data *data)
 		i++;
 	}
 	//convert_space_to_X(data->map_data.map);
-	if (check_valid_map(data) == false)
-		return (false);
+	// if (check_valid_map(data) == false)
+	// 	return (false);
 	return (true);
 }
 
