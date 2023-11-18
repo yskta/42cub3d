a339e9bf26343bbf8df0f85a8a4dc225f47e2f25
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2023-11-19 02:28:24 +0900

    debug

--- kitagawa_cub3d_untexture_ver_2/srcs/0_main.c
+++ kitagawa_cub3d_untexture_ver_2/srcs/0_main.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:20:59 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/11/19 02:10:52 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/11/19 02:27:57 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,25 @@ int main(int argc, char *argv[])
         put_error_and_exit("error in parse read data", data);
     if (init_other_data(data) == false)
         put_error_and_exit("error in init other data", data);
+    //仮で4_2_init_other_data.cの処理をここで行う
+    data->cur_pos = (t_cur_pos *)malloc(sizeof(t_cur_pos));
+	data->dir = (t_dir *)malloc(sizeof(t_dir));
+	data->plane = (t_plane *)malloc(sizeof(t_plane));
+	if (data->cur_pos == NULL || data->dir == NULL || data->plane == NULL)
+		return (false);
+    data->cur_pos->pos_x = 22;
+    data->cur_pos->pos_y = 12;
+    data->dir->dir_x = -1;
+    data->dir->dir_y = 0;
+    data->plane->plane_x = 0;
+    data->plane->plane_y = 0.66;
     printf("start raycasting\n");
     mlx_key_hook(data->mlx_win, key_hook, data);
     mlx_loop_hook(data->mlx, key_draw, data);
 	mlx_hook(data->mlx_win, DestroyNotify, ButtonPressMask, close_program, data);
 	mlx_loop(data->mlx);
     free_and_destroy_all(data);
+
     return (0);
 }
 
--- kitagawa_cub3d_untexture_ver_2/srcs/4_2_init_other_data.c
+++ kitagawa_cub3d_untexture_ver_2/srcs/4_2_init_other_data.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 19:04:18 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/11/19 01:56:19 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/11/19 02:27:11 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,32 @@
 //レイキャスティング用のデータを初期化する
 bool	init_pos_dir_plane(t_data *data)
 {
-	size_t	i;
-	size_t	j;
+	// size_t	i;
+	// size_t	j;
 
-	data->cur_pos = (t_cur_pos *)malloc(sizeof(t_cur_pos));
-	data->dir = (t_dir *)malloc(sizeof(t_dir));
-	data->plane = (t_plane *)malloc(sizeof(t_plane));
-	if (data->cur_pos == NULL || data->dir == NULL || data->plane == NULL)
-		return (false);
-	i = 0;
-	while (data->map_data.map[i] != NULL)
-	{
-		j = 0;
-		while (data->map_data.map[i][j] != '\0')
-		{
-			if (data->map_data.map[i][j] == 'N' || data->map_data.map[i][j] == 'S' || \
-			data->map_data.map[i][j] == 'E' || data->map_data.map[i][j] == 'W')
-			{
-				data->cur_pos->pos_x = (double)i;
-				data->cur_pos->pos_y = (double)j;
-				if (data->map_data.map[i][j] == 'N')
-				{
-					data->dir->dir_x = 0;
-					data->dir->dir_y = 1;
-					data->plane->plane_x = 0;
-					data->plane->plane_y = 0.66;
-				}
+	// data->cur_pos = (t_cur_pos *)malloc(sizeof(t_cur_pos));
+	// data->dir = (t_dir *)malloc(sizeof(t_dir));
+	// data->plane = (t_plane *)malloc(sizeof(t_plane));
+	// if (data->cur_pos == NULL || data->dir == NULL || data->plane == NULL)
+	// 	return (false);
+	// i = 0;
+	// while (data->map_data.map[i] != NULL)
+	// {
+	// 	j = 0;
+	// 	while (data->map_data.map[i][j] != '\0')
+	// 	{
+	// 		if (data->map_data.map[i][j] == 'N' || data->map_data.map[i][j] == 'S' || \
+	// 		data->map_data.map[i][j] == 'E' || data->map_data.map[i][j] == 'W')
+	// 		{
+	// 			data->cur_pos->pos_x = (double)i;
+	// 			data->cur_pos->pos_y = (double)j;
+	// 			if (data->map_data.map[i][j] == 'N')
+	// 			{
+	// 				data->dir->dir_x = 0;
+	// 				data->dir->dir_y = 1;
+	// 				data->plane->plane_x = 0;
+	// 				data->plane->plane_y = 0.66;
+	// 			}
 				// else if (data->map_data.map[i][j] == 'S')
 				// {
 				// 	data->dir->dir_x = 1;
@@ -62,18 +62,19 @@ bool	init_pos_dir_plane(t_data *data)
 				// 	data->plane->plane_x = -0.66;
 				// 	data->plane->plane_y = 0;
 				// }
-				break;
-			}
-			j++;
-		}
-		i++;
-	}
+	// 			break;
+	// 		}
+	// 		j++;
+	// 	}
+	// 	i++;
+	// }
 	// printf("for debug in init_pos_dir_plane\n");
 	// printf("data->dir->dir_x = %f\n", data->dir->dir_x);
 	// printf("data->dir->dir_y = %f\n", data->dir->dir_y);
 	// printf("data->plane->plane_x = %f\n", data->plane->plane_x);
 	// printf("data->plane->plane_y = %f\n", data->plane->plane_y);
-	data->map_data.map[(int)data->cur_pos->pos_x][(int)data->cur_pos->pos_y] = '0';
+	//TODO：外す必要あるかも
+	//data->map_data.map[(int)data->cur_pos->pos_x][(int)data->cur_pos->pos_y] = '0';
 	int k = 0;
 	while (data->map_data.map[k] != NULL)
 	{
