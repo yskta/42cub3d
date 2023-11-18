cf62a6bbd360d1f1ad4a7e25d15390ae764a4985
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2023-11-19 03:40:24 +0900

    push

--- copy_2_untexture/srcs/raycaster_draw.c
+++ copy_2_untexture/srcs/raycaster_draw.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:27:52 by snemoto           #+#    #+#             */
-/*   Updated: 2023/11/19 03:24:56 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/11/19 03:38:29 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,16 +110,16 @@ int	key_draw(t_vars *var)
 	{
 		var->camera_x = 2 * row / (double)SCREEN_W - 1;
 		calc_one(var);
-		printf ("data->camera_x:%f\n", var->camera_x);
-		printf ("data->ray_dir->ray_dir_x:%f\n", var->ray_dir->ray_dir_x);
-		printf ("data->ray_dir->ray_dir_y:%f\n", var->ray_dir->ray_dir_y);
+		// printf ("data->camera_x:%f\n", var->camera_x);
+		// printf ("data->ray_dir->ray_dir_x:%f\n", var->ray_dir->ray_dir_x);
+		// printf ("data->ray_dir->ray_dir_y:%f\n", var->ray_dir->ray_dir_y);
 		calc_two(var);
-		printf ("data->delta_dist->delta_dist_x:%f\n", var->delta_dist->delta_dist_x);
-		printf ("data->delta_dist->delta_dist_y:%f\n", var->delta_dist->delta_dist_y);
-		printf ("data->side_dist->side_dist_x:%f\n", var->side_dist->side_dist_x);
-		printf ("data->side_dist->side_dist_y:%f\n", var->side_dist->side_dist_y);
+		// printf ("data->delta_dist->delta_dist_x:%f\n", var->delta_dist->delta_dist_x);
+		// printf ("data->delta_dist->delta_dist_y:%f\n", var->delta_dist->delta_dist_y);
+		// printf ("data->side_dist->side_dist_x:%f\n", var->side_dist->side_dist_x);
+		// printf ("data->side_dist->side_dist_y:%f\n", var->side_dist->side_dist_y);
 		calc_three(var);
-		printf("var->perp_wall_dist:%f\n", var->perp_wall_dist);
+		//printf("var->perp_wall_dist:%f\n", var->perp_wall_dist);
 		draw_init(var);
 		col = var->draw_start;
 		while (col < (unsigned int)var->draw_end)
