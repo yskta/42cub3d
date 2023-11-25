5174e75032fdfb4612c2cdb0b9be948ce2beb95d
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2023-11-25 16:05:27 +0900

    3-4leftside-wallnorm対応

--- ForSubmit/srcs/3_4_parse_read_data.c
+++ ForSubmit/srcs/3_4_parse_read_data.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 12:31:24 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/11/25 15:57:55 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/11/25 16:05:00 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,48 @@ bool	checj_leftside_wall_zerocase(char **map, size_t i, \
 		return (true);
 }
 
+bool	check_leftside_wall_xcase(char **map, size_t i, \
+	size_t left_end_wall_index)
+{
+	while (map[i][left_end_wall_index + 1] == '1' && \
+		map[i + 1][left_end_wall_index] == 'X')
+		left_end_wall_index++;
+	if (map[i + 1][left_end_wall_index] != '1')
+		return (false);
+	else
+		return (true);
+}
+
+bool	check_leftside_wall_for_norm(char	**map, size_t i, \
+	size_t left_end_wall_index)
+{
+	size_t	i;
+
+	i = 0;
+	while (map[i + 1] != NULL)
+	{
+		if (map[i + 1][left_end_wall_index] == '0')
+		{
+			if (checj_leftside_wall_zerocase(map, i, \
+				left_end_wall_index) == false)
+				return (false);
+			else
+				i++;
+		}
+		else if (map[i + 1][left_end_wall_index] == 'X')
+		{
+			if (check_leftside_wall_xcase(map, i, \
+				left_end_wall_index) == false)
+				return (false);
+			else
+				i++;
+		}
+		else
+			i++;
+	}
+	return (true);
+}
+
 bool	check_leftside_wall(char	**map)
 {
 	size_t	i;
@@ -40,29 +82,7 @@ bool	check_leftside_wall(char	**map)
 	while (map[0][j] == 'X')
 		j++;
 	left_end_wall_index = j;
-	while (map[i + 1] != NULL)
-	{
-		if (map[i + 1][left_end_wall_index] == '0')
-		{
-			if (checj_leftside_wall_zerocase(map, i, left_end_wall_index) == false)
-				return (false);
-			else
-				i++;
-		}
-		else if (map[i + 1][left_end_wall_index] == 'X')
-		{
-			while (map[i][left_end_wall_index + 1] == '1' && \
-				map[i + 1][left_end_wall_index] == 'X')
-				left_end_wall_index++;
-			if (map[i + 1][left_end_wall_index] != '1')
-				return (false);
-			else
-				i++;
-		}
-		else
-			i++;
-	}
-	return (true);
+	return (check_leftside_wall_for_norm(map, i, left_end_wall_index));
 }
 
 bool	check_rightside_wall(char	**map)
