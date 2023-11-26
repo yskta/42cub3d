62242a26b826cc29e40359e1e685ddf45280b902
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2023-11-26 13:38:42 +0900

    test

--- ForSubmit/srcs/6_key_hook.c
+++ ForSubmit/srcs/6_key_hook.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 23:42:40 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/11/26 11:29:10 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/11/26 13:38:26 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,6 +101,6 @@ int	key_hook(int keycode, t_data *data)
 		close_program(data);
 	else
 		return (0);
-	key_draw_clear(data);
+	//key_draw_clear(data);
 	return (0);
 }
