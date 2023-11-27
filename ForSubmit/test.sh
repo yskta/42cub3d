3f4fa0b7cc9d43e8da1cb26555d9042bbb0ebfd7
Shota Nemoto <snemoto@c1r24s11.42tokyo.jp>
2023-11-27 18:31:01 +0900

    test.sh + add some map

---
+++ ForSubmit/test.sh
@@ -0,0 +1,183 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    test.sh                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: snemoto <snemoto@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/11/27 17:45:48 by snemoto           #+#    #+#              #
+#    Updated: 2023/11/27 18:27:10 by snemoto          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+make all
+
+# **************************************************************************** #
+
+VAL=id_color_1.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=id_color_2.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=id_color_3.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=id_color_4.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=id_color_5.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=id_color_6.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+# VAL=id_color_7.cub
+# echo ------------$VAL--------------
+# ./cub3D map/invalid_map/$VAL
+
+# VAL=id_color_8.cub
+# echo ------------$VAL--------------
+# ./cub3D map/invalid_map/$VAL
+
+VAL=id_color_9.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=id_color_10.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+# **************************************************************************** #
+
+VAL=id_dir_1.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=id_dir_2.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=id_dir_3.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=id_dir_4.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=id_dir_5.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=id_dir_6.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=id_dir_6.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+# **************************************************************************** #
+
+VAL=id_duplicate_1.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=id_duplicate_2.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+# **************************************************************************** #
+
+VAL=id_path_1.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=id_path_2.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=id_path_3.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+# **************************************************************************** #
+
+VAL=no_access.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=no_ext.txt
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=no_id.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+# VAL=no_map.cub
+# echo ------------$VAL--------------
+# ./cub3D map/invalid_map/$VAL
+
+# **************************************************************************** #
+
+VAL=pos_invalidchar.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=pos_multiplayer.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=pos_notplayable.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=wall_bottomline.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+# **************************************************************************** #
+
+VAL=wall_bottomline.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=wall_leftside_1.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=wall_leftside_2.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=wall_leftside_3.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=wall_rightside_1.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=wall_rightside_2.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=wall_rightside_3.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+VAL=wall_topline.cub
+echo ------------$VAL--------------
+./cub3D map/invalid_map/$VAL
+
+# **************************************************************************** #
+
+make fclean
