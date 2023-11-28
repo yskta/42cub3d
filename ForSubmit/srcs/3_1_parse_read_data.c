e03d0f28c76b4d21bfc43ff98788aa47daf0f477
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2023-11-28 15:05:51 +0900

    fix

--- ForSubmit/srcs/3_1_parse_read_data.c
+++ ForSubmit/srcs/3_1_parse_read_data.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 15:12:57 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/11/28 15:03:31 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/11/28 15:05:43 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,17 +111,17 @@ bool	parse_read_data(t_data *data)
 	data->ceiling.done_flag = false;
 	data->floor.done_flag = false;
 	parse_map(data);
-	//if (data->map_data.map_height < 3)
-	//{
-	//	free_map_data_and_path(data);
-	//	return (false);
-	//}
-	convert_space_to_x(data->map_data.map);
 	if (data->map_data.map_height < 3)
 	{
 		free_map_data_and_path(data);
 		return (false);
 	}
+	convert_space_to_x(data->map_data.map);
+	//if (data->map_data.map_height < 3)
+	//{
+	//	free_map_data_and_path(data);
+	//	return (false);
+	//}
 	if (parse_all_identifier(data) == false || \
 		data->north_path == NULL || data->south_path == NULL || \
 		data->west_path == NULL || data->east_path == NULL || \
